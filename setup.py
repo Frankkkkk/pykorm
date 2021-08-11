f8665bd95804e1ee74f5bc3f37f496e74ed163b2
Frank Villaro-Dixon <frank.villaro@infomaniak.com>
2021-08-11 18:31:10 +0200

    version: bump to 0.55.1
    Signed-off-by: Frank Villaro-Dixon <frank.villaro@infomaniak.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pykorm',
-    version='0.55.0',
+    version='0.55.1',
     description='Pykorm: a dead simple Kubernetes ORM',
     long_description=long_description,
     long_description_content_type='text/markdown',
